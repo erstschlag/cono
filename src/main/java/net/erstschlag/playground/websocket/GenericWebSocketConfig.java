3d9b790ed1cf971a99cef411e0220beecad08687
stefa <stefa@LAPTOP-N9AQTF0B.home>
2024-01-07 10:20:54 +0100

    REFACTOR move generic websocket config to websocket package

--- src/main/java/net/erstschlag/playground/websocket/GenericWebSocketConfig.java
+++ src/main/java/net/erstschlag/playground/websocket/GenericWebSocketConfig.java
@@ -1,4 +1,4 @@
-package net.erstschlag.playground;
+package net.erstschlag.playground.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
