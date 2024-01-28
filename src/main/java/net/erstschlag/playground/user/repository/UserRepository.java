a51cb613aa3fc9394910dd2d871ae63ede8b9549
stefa <stefa@LAPTOP-N9AQTF0B.home>
2024-01-28 20:51:48 +0100

    FIX reset of weekly lp

--- src/main/java/net/erstschlag/playground/user/repository/UserRepository.java
+++ src/main/java/net/erstschlag/playground/user/repository/UserRepository.java
@@ -4,10 +4,12 @@ import java.util.Collection;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, String> {
@@ -24,6 +26,8 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, S
 
     Page<UserEntity> findByNameLike(String name, Pageable pageable);
 
+    @Modifying
+    @Transactional
     @Query(value = "UPDATE twitch_user set weekly_lp = 0", nativeQuery = true)
     void resetWeeklyLP();
     
